721145fcf436cabbf78ad614feb01f7a4631deff
Renato Gaspar <renatomgaspars@gmail.com>
2025-09-16 22:49:51 +0100

    Order models

--- SuperShop/Controllers/ProductsController.cs
+++ SuperShop/Controllers/ProductsController.cs
@@ -87,7 +87,7 @@ namespace SuperShop.Controllers
         }
 
         // GET: Products/Edit/5
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -148,7 +148,7 @@ namespace SuperShop.Controllers
         }
 
         // GET: Products/Delete/5
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
