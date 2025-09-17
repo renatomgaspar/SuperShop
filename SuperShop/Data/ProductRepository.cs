baaa0e75360279514e9504f16d493c63330bbb45
Renato Gaspar <renatomgaspars@gmail.com>
2025-09-17 01:52:40 +0100

    Products combo

--- SuperShop/Data/ProductRepository.cs
+++ SuperShop/Data/ProductRepository.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SuperShop.Data.Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SuperShop.Data
 {
@@ -13,10 +15,28 @@ namespace SuperShop.Data
             _context = context;
         }
 
-        
+
         public IQueryable GetAllWithUsers()
         {
             return _context.Products.Include(p => p.User);
+        }
+
+        public IEnumerable<SelectListItem> GetComboProducts()
+        {
+            var list = _context.Products.Select(p => new SelectListItem
+            {
+                Text = p.Name,
+                Value = p.Id.ToString()
+            }).ToList();
+
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "(Select a product...)",
+                Value = "0"
+            });
+
+            return list;
         }
     }
 }
